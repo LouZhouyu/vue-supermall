a774f6604b83b9bcbb290da3806304afb3522e77
LouZhouyu <2651319339@qq.com>
2022-02-18 09:55:31 +0800

    chushih

---
+++ src/views/home/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="home">
+  </div>
+</template>
+
+<script>
+
+
+  export default {
+    name: "Home",
+    components: {
+    },
+    data() {
+      return {
+      }
+    },
+    created() {
+      // 1.请求多个数据
+
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
